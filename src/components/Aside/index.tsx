0a646746839d2f421e401bb866ffbf58ccc55750
Ricardo <ricardo10_2010@hotmail.com>
2020-10-11 17:45:14 -0300

    Aside estilizado

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
+import logoImg from '../../assets/logo.svg';
+import { Container, Header, LogImg, MenuContainer, MenuItemLink, Title } from './styles'
+import { MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp} from 'react-icons/md'
 
-import { Container } from './styles'
 
 const Aside: React.FC = () => {
     return (
         <Container>
-            <h1>Aside</h1>
+            <Header>
+                <LogImg src={logoImg} alt="Logo minha Carteira"/>
+                <Title>Minha Carteira</Title>
+            </Header>
+
+            <MenuContainer>
+                <MenuItemLink href="#">
+                    <MdDashboard/>
+                    Dashboard
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdArrowUpward/>
+                    Entradas
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdArrowDownward/>
+                    Saidas
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdExitToApp/>
+                    Sair
+                </MenuItemLink>
+            </MenuContainer>
         </Container>
     );
-        
 }
 
 export default Aside;
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -2,6 +2,50 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     grid-area: AS;
-    color:  ${props => props.theme.colors.white};
+    padding-left: 20px;
     background-color: ${props => props.theme.colors.secondary};
+    border-right: 1px solid ${props => props.theme.colors.gray};
+`;
+
+export const Header = styled.header`
+    height: 70px;
+    display: flex;
+    align-items: center;
+`;
+
+export const LogImg = styled.img`
+    width: 40px; 
+    height: 40px; 
+`;
+
+export const Title = styled.h3`
+    color: ${props => props.theme.colors.white};
+    margin-left: 10px;
+`;
+
+export const MenuContainer = styled.nav`
+    display:flex;
+    flex-direction: column;
+    margin-top: 50px;
+`;
+
+export const MenuItemLink = styled.a`
+    color: ${props => props.theme.colors.info};
+    text-decoration: none;
+    margin: 7px 0;
+    display: flex;
+    align-items: center;
+
+    transition: opacity .3s,
+                margin .3s;
+
+    &:hover {
+        opacity: .7;
+        margin: 7px 4px;
+    }
+
+    > svg {
+        font-size: 20px;
+        margin-right: 5px;
+    }
 `;
