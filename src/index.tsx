aac1a9e4e9a6600d4a8d72d6220f29ff52f4adab
Ricardo <ricardo10_2010@hotmail.com>
2020-10-11 00:12:43 -0300

    primeiro commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Dashboard from './pages/Dashboard'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Dashboard/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
