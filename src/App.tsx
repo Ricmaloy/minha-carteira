66316b47c23c5736d1f399915a5194fb11442730
Ricardo <ricardo10_2010@hotmail.com>
2020-10-12 01:17:54 -0300

    Adicionando Rotas e Navegação da Aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,17 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import Layout from './components/Layout';
 import GlobalStyles from './styles/GlobalStyles';
-//import Dashboard from './pages/Dashboard';
-import List from './pages/List';
 
 import dark from './styles/themes/dark';
 import light from './styles/themes/light';
+
+import Routes from './routes';
 
 const App: React.FC = () => {
     return (
         <ThemeProvider theme={dark}>
             <GlobalStyles/>
-            <Layout>
-                <List/>
-            </Layout>
+            <Routes/>
         </ThemeProvider>
     );
 }
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-
+import React, {useMemo} from 'react';
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 import { Container, Content, Filters } from './styles';
 
-const List: React.FC = () => {
+interface IRouteParams {
+    match: {
+        params: {
+            type: string,
+        }
+    }
+}
+
+const List: React.FC<IRouteParams> = ( {match} ) => {
+
+    const { type } = match.params;
+
+    const title = useMemo( () => {
+        return type === 'entry-balance' ? 'Entradas' : 'Saídas'
+    }, [type]);
+
+    const lineColor = useMemo( () => {
+        return type === 'entry-balance' ? '#F7931B' : '#E44C4E'
+    }, [type]);
+
 
     const months = [
         {value: 7, label: 'Julho' },
@@ -21,7 +39,7 @@ const List: React.FC = () => {
 
     return (
         <Container>
-            <ContentHeader title="Saidas" lineColor="#E44C4E">
+            <ContentHeader title={title} lineColor={lineColor}>
                 <SelectInput options={months}/>
                 <SelectInput options={years}/>
             </ContentHeader>
