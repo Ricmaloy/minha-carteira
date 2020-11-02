c33eb1befec8d32d45bb0cacbbe47fffd91a7e2a
Ricardo <ricardo10_2010@hotmail.com>
2020-11-02 09:05:29 -0300

    History Expenses & Gains LineChart

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -5,6 +5,7 @@ import SelectInput from '../../components/SelectInput';
 import WalletBox from '../../components/WalletBox';
 import MessageBox from '../../components/MessageBox';
 import PieChartBox from '../../components/PieChartBox';
+import HistoryBox from '../../components/HistoryBox';
 
 import gains from '../../repositories/gains';
 import expenses from '../../repositories/expenses';
@@ -153,6 +154,54 @@ const Dashboard: React.FC = () => {
 
     }, [totalBudget.totalInc, totalBudget.totalExp]);
 
+    const historyData = useMemo(() => {
+        return listOfMonths.map((_, month) => {
+            
+            let amountEntry = 0;
+            gains.forEach(gain => {
+                const date = new Date(gain.date);
+                const gainMonth = date.getMonth();
+                const gainYear = date.getFullYear();
+
+                if(gainMonth === month && gainYear === yearSelected){
+                    try {
+                        amountEntry += Number(gain.amount)
+                    } catch {
+                        throw new Error('amountEntry is invalid. amountEntry must be valid number!');
+                    }
+                }
+            });
+
+            let amountOutput = 0;
+            expenses.forEach(expense => {
+                const date = new Date(expense.date);
+                const expenseMonth = date.getMonth();
+                const expenseYear = date.getFullYear();
+
+                if(expenseMonth === month && expenseYear === yearSelected){
+                    try {
+                        amountOutput += Number(expense.amount)
+                    } catch {
+                        throw new Error('amountOutput is invalid. amountOutput must be valid number!');
+                    }
+                }
+            });
+
+            return {
+                monthNumber: month,
+                month: listOfMonths[month].substr(0,3),
+                amountEntry,
+                amountOutput
+            }
+        })
+        .filter(item => {
+            const currentMonth = new Date().getMonth();
+            const currentYear = new Date().getFullYear();
+
+            return (yearSelected === currentYear && item.monthNumber <= currentMonth) || (yearSelected < currentYear)
+        });
+    },[yearSelected]);
+
     const handleMonthSelected = (month: string) => {
         try {
             const parseMonth = Number(month);
@@ -216,6 +265,12 @@ const Dashboard: React.FC = () => {
                 />
 
                 <PieChartBox data={relationExpVsGains} />
+
+                <HistoryBox
+                    data={historyData}
+                    lineColorAmountEntry="#f7931b"
+                    lineColorAmountOutput="#e44c4e"
+                />
             </Content>
         </Container>
     );
